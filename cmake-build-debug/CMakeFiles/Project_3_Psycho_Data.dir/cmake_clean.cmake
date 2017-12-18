c192c3fd0631a26ed9b35c62db768bb854a541ff
Gabriel Gauci <gabrielgauci@gmail.com>
2017-12-18 10:28:23 +0100

    no message

---
+++ cmake-build-debug/CMakeFiles/Project_3_Psycho_Data.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project_3_Psycho_Data.dir/main.c.obj"
+  "CMakeFiles/Project_3_Psycho_Data.dir/calculator.c.obj"
+  "Project_3_Psycho_Data.pdb"
+  "Project_3_Psycho_Data.exe"
+  "Project_3_Psycho_Data.exe.manifest"
+  "libProject_3_Psycho_Data.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Project_3_Psycho_Data.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
